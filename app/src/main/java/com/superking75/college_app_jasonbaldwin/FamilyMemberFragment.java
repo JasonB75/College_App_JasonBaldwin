f43dc10f2a343ce1c4756579ddfa95a7015844db
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2019-03-27 10:36:35 -0400

    Initial commit

---
+++ app/src/main/java/com/superking75/college_app_jasonbaldwin/FamilyMemberFragment.java
@@ -0,0 +1,19 @@
+package com.superking75.college_app_jasonbaldwin;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FamilyMemberFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup view, Bundle bundle)
+    {   super.onCreateView(inflater,view,bundle);
+        return view;
+    }
+
+
+
+}
