2d391bb1c54e658b78e2561c16da85e1294d55ae
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2019-04-02 09:49:17 -0400

    DatePicker

--- app/src/main/java/com/superking75/college_app_jasonbaldwin/DatePickerFragment.java
+++ app/src/main/java/com/superking75/college_app_jasonbaldwin/DatePickerFragment.java
@@ -1,28 +1,36 @@
 package com.superking75.college_app_jasonbaldwin;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.DatePicker;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DatePickerFragment extends DialogFragment
 {
     final static String DATE_ARGUMENT = "dateofbirth";
+    final static String EXTRA_DATE_OF_BIRTH = "com.jasonb75.college_app_jasonbaldwin.DatePickerFragment";
     DatePicker mDatePicker;
     Date mDate;
-    Calendar mCalander; 
+    Calendar mCalander;
+
     @Override
     public AlertDialog onCreateDialog(Bundle budle)
     {
         View v = LayoutInflater.from(getActivity())
                 .inflate(R.layout.fragment_date_picker, null);
-      //  mDate = (Date)getArguments.setSerializable(DATE_ARGUMENT);
-      //  mCalander.Calendar.getInstance();
+       mDate = (Date)getArguments.setSerializable(DATE_ARGUMENT);
+       mCalander.Calendar.getInstance();
+       mCalendar.setTime(mDate);
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
                 .setTitle("Date of Birth")
